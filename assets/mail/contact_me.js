ce4498ca75a3dfb96ec351edd0ebe52cfd818daf
Rorisang21 <mahlangurorisang1@gmail.com>
2023-10-13 16:18:48 +0200

    changes have been made

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://mc6e8ua7sj.execute-api.eu-west-1.amazonaws.com/DEV_stage",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
